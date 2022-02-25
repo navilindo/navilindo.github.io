cbef8be87769d7fafbf1df0227b87b8973f90905
navilindo <hassainjr@gmail.com>
2022-02-25 21:05:50 +0200

    height for abt

--- res/js/index.js
+++ res/js/index.js
@@ -2,4 +2,10 @@
 $( document ).click(function() {
   $( "#toggle" ).toggle( "slide" );
 });
+
+function showAbout() {
+    $(#aboutBtn).click(function(){
+        $(#about-me).toggle( "slide" );
+    });    
+}   
 </script>
