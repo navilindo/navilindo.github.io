e476abad698234fbb6b94b07259db945d552833e
navilindo <hassainjr@gmail.com>
2022-02-26 15:16:40 +0200

    link to home.html into typing

--- res/js/typing.js
+++ res/js/typing.js
@@ -3,7 +3,7 @@ function type(){
 var p = document.getElementById('loading-text');
 p.innerHTML = '';
 var n = 0;
-var str = ' \>_ <br> There is no one who is mindful of what he says except that you will see the good of it in the rest of his actions. <br> <br> >_ <span id="icon-holder"> <a href="#home" onclick="goHome()"> <i class="bi bi-hexagon-fill text-white" id="load-home icon-2"></i>&nbsp; visit navilindo</a> </span>';
+var str = ' \>_ <br> There is no one who is mindful of what he says except that you will see the good of it in the rest of his actions. <br> <br> >_ <span id="icon-holder"> <a href="home.html#home" onclick="goHome()"> <i class="bi bi-hexagon-fill text-white" id="load-home icon-2"></i>&nbsp; visit navilindo</a> </span>';
 var typeTimer = setInterval(function() {
   n = n + 1;
   p.innerHTML = " " + str.slice(0, n);
