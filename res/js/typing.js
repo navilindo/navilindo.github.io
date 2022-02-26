91f4a274d534c8d95a11386a31c0a41adc440ca3
navilindo <hassainjr@gmail.com>
2022-02-26 08:23:31 +0200

    pretype & post-type styles

--- res/js/typing.js
+++ res/js/typing.js
@@ -1,7 +1,7 @@
 var p = document.getElementById('loading-text');
 p.innerHTML = '';
 var n = 0;
-var str = ' \>_" There is no one who is mindful of what he says except that you will see <br> the good of it in the rest of his actions.\" <br> <b><u>Yūnus b. ʿUbayd</u> </b> - Rahimahullaah ';
+var str = ' \>_ <br> There is no one who is mindful of what he says except that you will see the good of it in the rest of his actions. <br> <br> >_ Yūnus b. ʿUbayd';
 var typeTimer = setInterval(function() {
   n = n + 1;
   p.innerHTML = " " + str.slice(0, n);
